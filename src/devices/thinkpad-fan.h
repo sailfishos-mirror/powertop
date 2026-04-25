16c0e2d13efba7da9117d0a6ff179d920a5b987f
Arjan van de Ven <arjanvandeven@gmail.com>
2026-04-25 08:51:21 -0700

    Refactor: Add _s() string methods to thinkpad_fan

--- src/devices/thinkpad-fan.h
+++ src/devices/thinkpad-fan.h
@@ -45,7 +45,9 @@ public:
 	virtual const char * class_name(void) { return "fan";};
 
 	virtual const char * device_name(void) { return "Fan-1";};
+	virtual std::string device_name_s(void) { return human_name(); };
 	virtual const char * human_name(void) { return "Laptop fan";};
+	virtual std::string human_name_s(void) { return human_name(); };
 	virtual double power_usage(struct result_bundle *result, struct parameter_bundle *bundle);
 	virtual const char * util_units(void) { return " rpm"; };
 	virtual int power_valid(void) { return utilization_power_valid(r_index);};
