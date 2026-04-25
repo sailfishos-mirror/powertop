81bba99c0bad4d5b04767fc74aa7fe0c34cd3dac
Arjan van de Ven <arjanvandeven@gmail.com>
2026-04-25 09:07:34 -0700

    Refactor: Add _s() string methods to gpu_rapl_device

--- src/devices/gpu_rapl_device.h
+++ src/devices/gpu_rapl_device.h
@@ -46,6 +46,8 @@ public:
 	gpu_rapl_device(i915gpu *parent);
 	virtual const char * class_name(void) { return "GPU core";};
 	virtual const char * device_name(void) { return "GPU core";};
+	virtual std::string device_name_s(void) { return "GPU core";};
+	virtual std::string human_name_s(void) { return "GPU core";};
 	bool device_present() { return device_valid;}
 	virtual double power_usage(struct result_bundle *result, struct parameter_bundle *bundle);
 	virtual void start_measurement(void);
